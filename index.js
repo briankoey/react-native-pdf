405f4cbd4fd3b0915b5596ef791265f42e80da09
Brian Koey <brian.koey@myrepublic.net>
2019-05-22 21:37:39 +0800

    compatible with aws invoice link

--- index.js
+++ index.js
@@ -252,7 +252,8 @@ export default class Pdf extends Component {
         })
             .fetch(
                 source.method ? source.method : 'GET',
-                encodeURI(decodeURI(source.uri)),
+                // encodeURI(decodeURI(source.uri)),
+                source.uri,
                 source.headers ? source.headers : {},
                 source.body ? source.body : ""
             )
